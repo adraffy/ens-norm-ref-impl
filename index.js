575f0d3080f8c09bc10d52b6ecd0f2ef5286404b
Andrew Raffensperger <raffy@me.com>
2022-08-04 00:46:14 -0700

    simplify emoji parser

--- index.js
+++ index.js
@@ -30,23 +30,13 @@ function consume_emoji(cps) {
 	let emoji;
 	let node = emoji_root;
 	let pos = cps.length;
-	let fe0f;
 	while (pos) {
 		let cp = cps[--pos];
-		if (cp == 0xFE0F) {
-			if (!fe0f) break; // we didn't expect FE0F
-			fe0f = false; // clear flag
-			continue;
-		}
 		node = node[cp];
 		if (!node) break;
-		fe0f = node.__fe0f;
+		if (node.__fe0f && pos > 0 && cps[pos - 1] == 0xFE0F) pos--;
 		if (node.__valid) { // this is a valid emoji (so far)
-			emoji = node.__valid;
-			if (fe0f && pos > 0 && cps[pos - 1] == 0xFE0F) { // eat FE0F too
-				fe0f = false;
-				pos--;
-			}
+			emoji = node.__valid; // remember it
 			cps.length = pos; // remove it from input
 		}
 	}
