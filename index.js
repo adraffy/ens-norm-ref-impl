a9c104c87a11fe4c07458acf0f69cd0ff36b7a2a
Andrew Raffensperger <raffy@me.com>
2022-09-01 02:06:25 -0700

    update ensip, added cm logic

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ import {CHARS, EMOJI} from '@adraffy/ensip-norm';
 
 const HYPHEN = 0x2D;
 const UNDERSCORE = 0x5F;
+const FE0F = 0xFE0F;
 
 // create lookup tables
-let {valid, mapped, ignored} = CHARS;
+let {valid, mapped, ignored, cm} = CHARS;
 valid = new Set(valid);
 ignored = new Set(ignored);
 mapped = Object.fromEntries(mapped);
+cm = new Set(cm);
 
 // create simple trie 
 let emoji_root = {};
 for (let emoji of EMOJI) {
 	let node = emoji_root;
 	for (let cp of emoji) {
-		if (cp === 0xFE0F) {
+		if (cp === FE0F) {
 			node.__fe0f = true;
 			continue;
 		}		
@@ -29,7 +31,7 @@ for (let emoji of EMOJI) {
 // find longest emoji match
 // allow optional FE0F
 // returns the full sequence
-function consume_emoji(cps) {
+function consume_emoji_reversed(cps) {
 	let emoji;
 	let node = emoji_root;
 	let pos = cps.length;
@@ -37,7 +39,7 @@ function consume_emoji(cps) {
 		let cp = cps[--pos];
 		node = node[cp];
 		if (!node) break;
-		if (node.__fe0f && pos > 0 && cps[pos - 1] == 0xFE0F) pos--;
+		if (node.__fe0f && pos > 0 && cps[pos - 1] == FE0F) pos--;
 		if (node.__valid) { // this is a valid emoji (so far)
 			emoji = node.__valid; // remember it
 			cps.length = pos; // remove it from input
@@ -46,39 +48,17 @@ function consume_emoji(cps) {
 	return emoji;
 }
 
-// split string into codepoints
 function explode_cp(s) {
 	return [...s].map(c => c.codePointAt(0));
 }
 
-// check leading underscore
-// check label extension (if ASCII)
-function post_check(name) {
-	for (let label of name.split('.')) {		
-		let cps = explode_cp(label);
-		try {
-			for (let i = cps.lastIndexOf(UNDERSCORE) - 1; i >= 0; i--) {
-				if (cps[i] !== UNDERSCORE) {
-					throw new Error(`underscore only allowed at start`);
-				}
-			}
-			if (cps.length >= 4 && cps.every(cp => cp < 0x80) && cps[2] === HYPHEN && cps[3] === HYPHEN) {
-				throw new Error(`invalid label extension`);
-			}
-		} catch (err) {
-			throw new Error(`Invalid label "${label}": ${err.message}`);
-		}
-	}
-}
-
-// follow ENSIP processing steps directly
-function normalize(name, emoji_filter) {
+function process(name, emoji_filter) {
 	let input = explode_cp(name).reverse(); // flip so we can pop
 	let output = [];
-	while (input.length) {		
-		let emoji = consume_emoji(input);
+	while (input.length) {
+		let emoji = consume_emoji_reversed(input);
 		if (emoji) {
-			output.push(...emoji_filter(emoji));
+			output.push(...emoji_filter(emoji)); 
 			continue;
 		}
 		let cp = input.pop();
@@ -96,14 +76,45 @@ function normalize(name, emoji_filter) {
 		}
 		throw new Error(`Disallowed codepoint: 0x${cp.toString(16).toUpperCase()}`);
 	}
-	let norm = String.fromCodePoint(...output).normalize('NFC');
-	post_check(norm);
+	return String.fromCodePoint(...output);
+}
+
+function post_check(name, emoji_filter) {
+	let norm = process(name, emoji_filter).normalize('NFC');
+	for (let label of norm.split('.')) {
+		try {
+			let nfc = explode_cp(label);
+			for (let i = nfc.lastIndexOf(UNDERSCORE) - 1; i >= 0; i--) {
+				if (nfc[i] !== UNDERSCORE) {
+					throw new Error(`underscore only allowed at start`);
+				}
+			}
+			if (nfc.length >= 4 && nfc[2] === HYPHEN && nfc[3] === HYPHEN && nfc.every(cp => cp < 0x80)) {
+				throw new Error(`invalid label extension`);
+			}			
+			let nfd = explode_cp(process(label, () => [FE0F]).normalize('NFD'));
+			for (let i = 0, j = -1; i < nfd.length; i++) {
+				if (cm.has(nfd[i])) {
+					if (i == 0) {
+						throw new Error(`leading combining mark`);
+					} else if (i == j) {
+						throw new Error(`adjacent combining marks`);
+					} else if (nfd[i - 1] == FE0F) {
+						throw new Error(`emoji + combining mark`);
+					}	
+					j = i + 1;
+				}
+			}
+		} catch (err) {
+			throw new Error(`Invalid label "${label}": ${err.message}`);
+		}
+	}
 	return norm;
 }
 
-export function ens_normalize(name) {
-	return normalize(name, e => e.filter(cp => cp != 0xFE0F));
-}
 export function ens_beautify(name) {
-	return normalize(name, e => e);
+	return post_check(name, emoji => emoji);
+}
+export function ens_normalize(name) {
+	return post_check(name, emoji => emoji.filter(cp => cp != FE0F));
 }
